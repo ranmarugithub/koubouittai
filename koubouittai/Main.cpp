dd5c733c753b14aacfd9ca73aadbb2ab0fe593b8
ranmarugithub <kokuri38ranmaru@gmail.com>
2023-10-02 15:52:58 +0900

    test

--- koubouittai/Main.cpp
+++ koubouittai/Main.cpp
@@ -1,23 +1,123 @@
-﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.11
+﻿# include <Siv3D.hpp>
 
 void Main()
 {
-	// 背景の色を設定する | Set the background color
-	Scene::SetBackground(Palette::Midnightblue);
-	Texture submarine(U"⛵"_emoji);
+	Circle circle;
+
+	double directionx = 0.0;
+	double directiony = 0.0;
+	bool shotenable = false;
+
+	Vec2 pos{ 400, 300 };
+
+	const double speed = 200;
 
 	while (System::Update())
 	{
-		submarine.drawAt(400, 300);
+		ClearPrint();
+
+		const double deltaTime = Scene::DeltaTime();
+
+		//円を描く
+		Circle(pos, 20).draw(Palette::White);
+
+		//弾を発射
+		if (KeySpace.down())
+		{
+			shotenable = true;
+		}
+
+		//弾の移動入力
+		// ←
+		if (KeyA.pressed())
+		{
+			if (directionx > -1.0)
+			{
+				directionx -= 0.01;
+			}
+
+			if (directiony > 0)
+			{
+				directiony -= 0.005;
+			}
+			if (directiony < 0)
+			{
+				directiony += 0.005;
+			}
+		}
+
+		// → 
+		if (KeyD.pressed())
+		{
+			if (directionx < 1.0)
+			{
+				directionx += 0.01;
+			}
+
+			if (directiony > 0)
+			{
+				directiony -= 0.005;
+			}
+			if (directiony < 0)
+			{
+				directiony += 0.005;
+			}
+		}
+
+		// ↑ 
+		if (KeyW.pressed())
+		{
+			if (directiony > -1.0)
+			{
+				directiony -= 0.01;
+			}
+
+			if (directionx > 0)
+			{
+				directionx -= 0.005;
+			}
+			if (directionx < 0)
+			{
+				directionx += 0.005;
+			}
+		}
+
+		// ↓ 
+		if (KeyS.pressed())
+		{
+			if (directiony < 1.0)
+			{
+				directiony += 0.01;
+			}
+
+			if (directionx > 0)
+			{
+				directionx -= 0.005;
+			}
+			if (directionx < 0)
+			{
+				directionx += 0.005;
+			}
+		}
+
+		if (shotenable == true)
+		{
+			//弾の移動
+			pos.x += (speed * deltaTime * directionx);
+			pos.y += (speed * deltaTime * directiony);
+
+			//弾を無効にする
+			if (pos.x < 0 || pos.x > 800 || pos.y < 0 || pos.y > 600)
+			{
+				shotenable = false;
+				pos = { 400, 300 };
+			}
+		}
+
+		//デバッグ
+		Print << U"{:.4f}"_fmt(pos);
+		Print << U"{:.4f}"_fmt(directionx);
+		Print << U"{:.4f}"_fmt(directiony);
+		Print << shotenable;
 	}
 }
-
-//
-// - Debug ビルド: プログラムの最適化を減らす代わりに、エラーやクラッシュ時に詳細な情報を得られます。
-//
-// - Release ビルド: 最大限の最適化でビルドします。
-//
-// - [デバッグ] メニュー → [デバッグの開始] でプログラムを実行すると、[出力] ウィンドウに詳細なログが表示され、エラーの原因を探せます。
-//
-// - Visual Studio を更新した直後は、プログラムのリビルド（[ビルド]メニュー → [ソリューションのリビルド]）が必要な場合があります。
-//
